603ffc2f1934345011c9fa9b5be24997ab8d00dd
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-12 17:40:29 +0200

    middleware console log after api request

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+const logger = (req, res, next) => {
+  req.hello = 'Hello World';
+  console.log('Middleware ran');
+  next();
+};
+app.use(logger);
+
+
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
