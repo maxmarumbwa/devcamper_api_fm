510f51c218e3eb61d921b7917fba3267f91409ea
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-12 16:14:28 +0200

    print test all routes versioning

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-// Load env vars
+// Load environment variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-// Define a test route
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
+// Define routes
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
 });
 
-const PORT = process.env.PORT || 5000;
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
 
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
