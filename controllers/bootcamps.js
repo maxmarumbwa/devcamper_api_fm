9909937fad9321099290f64f353e98eae48579aa
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-15 22:33:42 +0200

    custom error respose class- print id not found

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,6 @@
 const Bootcamp = require('../models/Bootcamp');
 const slugify = require('slugify');
-
+const ErrorResponse = require('../utils/errorResponse');
 
 
 // @desc    Get all bootcamps
@@ -25,18 +25,17 @@ exports.getBootcamps = async (req, res, next) => {
 // @route     GET /api/v1/bootcamps/:id
 // @access    Public
 exports.getBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.findById(req.params.id);
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        
+        if (!bootcamp) {
+            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+        }
 
-    if (!bootcamp) {
-      return res.status(400).json({ success: false });
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (error) {
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
-
-    res.status(200).json({ success: true, data: bootcamp });
-  } catch (err) {
-   // res.status(400).json({ success: false });
-    next(err);
-  }
 };
 
 
--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,13 @@
-const errorHandler = (err, req, res, next) => {
-  // Log to console for dev
-  console.log(err.stack.red);
 
-  res.status(500).json({
-    success: false,
-    error: err.message
-  });
+const errorHandler = (err, req, res, next) => {
+    // Log the error stack to the console for development
+    console.log(err.stack.red);
+
+    // Set the response status code, defaulting to 500 if err.statusCode is not present
+    res.status(err.statusCode || 500).json({
+        success: false,
+        error: err.message || 'Server Error',
+    });
 };
 
 module.exports = errorHandler;
