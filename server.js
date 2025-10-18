eda6ce78ab37bd736be52d7bd8f6a08506c788d5
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-19 01:17:05 +0200

    get all courses route

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
-
+const courses = require('./routes/courses');
 
 const app = express();
 
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
 
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
-
+app.use('/api/v1/courses', courses);
 app.use(errorHandler);
 
 // Start server
