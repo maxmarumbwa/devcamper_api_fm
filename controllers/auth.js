2cc5bf6ee341f4b53a63ae8cb167c1d87f406b21
maxmarumbwa <maxmarumbwa@gmail.com>
2025-10-20 07:19:32 +0200

    login route auth and jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const asynchandler = require('../middleware/async');
 const User = require('../models/User');
 
+
 // @desc    Register user
 // @route    GET /api/v1/auth/register
 // @access    Public
@@ -20,3 +21,48 @@ const token = user.getSignedJwtToken();
 
     res.status(200).json({ succes: true, token});
 });
+
+
+// @desc      Login user
+// @route     POST /api/v1/auth/login
+// @access    Public
+exports.login = asynchandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate emil & password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check for user
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credentials', 401));
+  }
+
+   // ✅ Generate token directly
+  const token = user.getSignedJwtToken();
+  res.status(200).json({ success: true, token });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
